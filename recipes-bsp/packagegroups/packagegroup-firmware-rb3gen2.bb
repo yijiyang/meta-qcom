yijiyang/meta-qcom

commit 93baffeda42eb08045cba4cc25218da299d91ec1

packagegroup-firmware-rb3gen2: add packagegroups for rb3gen2 firmware packages
Add packagegroup for RB3Gen2, recommending board firmware packages.

Signed-off-by: Atul Dhudase <quic_adhudase@quicinc.com>

recipes-bsp/packagegroups/packagegroup-firmware-rb3gen2.bb
@@ -1,0 +1,13 @@
+SUMMARY = "Firmware packages for the RB3Gen2 platform"
+
+inherit packagegroup
+
+RRECOMMENDS:${PN} += " \
+    firmware-qcom-rb3gen2 \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'opengl', 'linux-firmware-qcom-adreno-a660', '', d)} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wifi', 'linux-firmware-ath11k', '', d)} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'bluetooth', 'linux-firmware-qca', '', d)} \
+    linux-firmware-qcom-qcs6490-audio \
+    linux-firmware-qcom-qcs6490-compute \
+    linux-firmware-qcom-vpu-2.0 \
+"

commit 7b00c31320502357a8299a661bffe84d6ce2d5f6

packagegroup-firmware-rb3gen2: add Adreno ZAP binary
Add Adreno Zap shader to the list of RB3 Gen2 firmware packages.

Fixes: 93baffeda42e ("packagegroup-firmware-rb3gen2: add packagegroups for rb3gen2 firmware packages")
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

recipes-bsp/packagegroups/packagegroup-firmware-rb3gen2.bb
@@ -3,7 +3,7 @@ SUMMARY = "Firmware packages for the RB3Gen2 platform"
 inherit packagegroup
 
 RRECOMMENDS:${PN} += " \
-    ${@bb.utils.contains('DISTRO_FEATURES', 'opengl', 'linux-firmware-qcom-adreno-a660', '', d)} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'opengl', 'linux-firmware-qcom-adreno-a660 linux-firmware-qcom-qcm6490-adreno', '', d)} \
     ${@bb.utils.contains('DISTRO_FEATURES', 'wifi', 'linux-firmware-ath11k-wcn6750 linux-firmware-qcom-qcm6490-wifi', '', d)} \
     ${@bb.utils.contains('DISTRO_FEATURES', 'bluetooth', 'linux-firmware-qca-wcn6750', '', d)} \
     linux-firmware-qcom-qcm6490-audio \