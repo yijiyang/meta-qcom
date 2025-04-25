yijiyang/meta-qcom

commit ebf31685517982976e38b3e9324b0121314f28bc

packagegroups: Define QCOM userspace helper packagegroups
Define package groups with essential and additional userspace pkgs
required on multiple QCOM machines to avoid repeatedly adding them
in each machine configuration.

Signed-off-by: Viswanath Kraleti <quic_vkraleti@quicinc.com>

recipes-bsp/packagegroups/packagegroup-qcom.bb
@@ -1,0 +1,19 @@
+SUMMARY = "Extra userspace packages for QCOM platforms"
+
+inherit packagegroup
+
+PACKAGES = " \
+    ${PN}-boot-essential \
+    ${PN}-boot-additional \
+"
+
+RRECOMMENDS:${PN}-boot-essential = " \
+    pd-mapper \
+    qrtr \
+    rmtfs \
+    tqftpserv \
+"
+
+RRECOMMENDS:${PN}-boot-additional = " \
+    fastrpc \
+"

conf/machine/include/qcom-sa8155p.inc
@@ -4,12 +4,9 @@ DEFAULTTUNE = "armv8-2a-crypto"
 require conf/machine/include/arm/arch-armv8-2a.inc
 
 MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += " \
-    pd-mapper \
-    qrtr \
-    rmtfs \
-    tqftpserv \
+    packagegroup-qcom-boot-essential \
 "
 
 MACHINE_EXTRA_RRECOMMENDS += " \
-    fastrpc \
+    packagegroup-qcom-boot-additional \
 "

conf/machine/include/qcom-sdm845.inc
@@ -4,12 +4,9 @@ DEFAULTTUNE = "armv8-2a-crypto"
 require conf/machine/include/arm/arch-armv8-2a.inc
 
 MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += " \
-    pd-mapper \
-    qrtr \
-    rmtfs \
-    tqftpserv \
+    packagegroup-qcom-boot-essential \
 "
 
 MACHINE_EXTRA_RRECOMMENDS += " \
-    fastrpc \
+    packagegroup-qcom-boot-additional \
 "

conf/machine/include/qcom-sm8250.inc
@@ -4,12 +4,9 @@ DEFAULTTUNE = "armv8-2a-crypto"
 require conf/machine/include/arm/arch-armv8-2a.inc
 
 MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += " \
-    pd-mapper \
-    qrtr \
-    rmtfs \
-    tqftpserv \
+    packagegroup-qcom-boot-essential \
 "
 
 MACHINE_EXTRA_RRECOMMENDS += " \
-    fastrpc \
+    packagegroup-qcom-boot-additional \
 "

conf/machine/qcom-armv8a.conf
@@ -52,14 +52,11 @@ KERNEL_CMDLINE_EXTRA[sdm845-db845c] ?= "clk_ignore_unused pd_ignore_unused"
 
 # Userspace tools
 MACHINE_ESSENTIAL_EXTRA_RRECOMMENDS += " \
-    pd-mapper \
-    qrtr \
-    rmtfs \
-    tqftpserv \
+    packagegroup-qcom-boot-essential \
 "
 
 MACHINE_EXTRA_RRECOMMENDS += " \
-    fastrpc \
+    packagegroup-qcom-boot-additional \
 "
 
 # Modules and firmware for all supported machines

recipes-test/images/initramfs-tiny-image.bb
@@ -10,11 +10,8 @@ PACKAGE_INSTALL = " \
 
 PACKAGE_INSTALL += " \
     debugcc \
-    fastrpc \
-    pd-mapper \
-    qrtr \
-    rmtfs \
-    tqftpserv \
+    packagegroup-qcom-boot-additional \
+    packagegroup-qcom-boot-essential \
 "
 
 # Do not pollute the initrd image with rootfs features

commit c5b4e0e143b73c9552b549be2d2c3d19f74526bf

packagegroup-qcom: use RDEPENDS instead of RRECOMMENDS
All initramfs-*-image recipes use NO_RECOMMENDS to keep the size
minimal. The commit ebf3168 ("packagegroups: Define QCOM userspace
helper packagegroups") made initramfs-tiny-image to pull
packagegroup-qcom-* packages, which RRECOMMENDS platform packages
instead of RDEPENDing on them, thus all testing initramfs images have
lost corresponding Qualcomm tools.

Make packagegroup-qcom use RDEPENDS instead of RRECOMMENDS in order to
brign tool packages back.

Fixes: ebf316855179 ("packagegroups: Define QCOM userspace helper packagegroups")
Suggested-by: Viswanath Kraleti <vkraleti@oss.qualcomm.com>
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

recipes-bsp/packagegroups/packagegroup-qcom.bb
@@ -7,13 +7,13 @@ PACKAGES = " \
     ${PN}-boot-additional \
 "
 
-RRECOMMENDS:${PN}-boot-essential = " \
+RDEPENDS:${PN}-boot-essential = " \
     pd-mapper \
     qrtr \
     rmtfs \
     tqftpserv \
 "
 
-RRECOMMENDS:${PN}-boot-additional = " \
+RDEPENDS:${PN}-boot-additional = " \
     fastrpc \
 "