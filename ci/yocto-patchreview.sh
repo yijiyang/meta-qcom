68eaa875b5cc9c78634a768c64efede2e933316c
Jose Quaresma <jose.quaresma@oss.qualcomm.com>
2025-06-17 18:31:52 +0100

    ci: build-yocto: switch to kas-shell-helper
    Adapt all ci/scripts as now the TOPDIR and KAS_WORK_DIR are received
    as an argument from the kas-shell-helper:
    - REPO_DIR is the previous TOPDIR
    - WORK_DIR is the previous KAS_WORK_DIR
    
    Signed-off-by: Jose Quaresma <jose.quaresma@oss.qualcomm.com>

--- ci/yocto-patchreview.sh
+++ ci/yocto-patchreview.sh
@@ -2,15 +2,28 @@
 # Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-TOPDIR=$(realpath $(dirname $(readlink -f $0))/..)
+if [ -z $1 ] || [ -z $2 ] ; then
+    echo "The REPO_DIR or WORK_DIR is empty and it needs to point to the corresponding directories."
+    echo "Please run it with:"
+    echo " $0 REPO_DIR WORK_DIR"
+    exit 1
+fi
 
-# Ensure KAS workspace is outside of the checked out repo
-# Allows the caller to specify KAS_WORK_DIR, otherwise make temp one
-export KAS_WORK_DIR=$(realpath ${KAS_WORK_DIR:-$(mktemp -d)})
+REPO_DIR="$1"
+WORK_DIR="$2"
 
-echo "Running kas in $KAS_WORK_DIR"
-kas shell $TOPDIR/ci/base.yml --command "$KAS_WORK_DIR/oe-core/scripts/contrib/patchreview.py -v -b -j status.json $TOPDIR"
+_is_dir(){
+    test -d "$1" && return
+    echo "The '$1' is not a directory."
+    exit 1
+}
+
+_is_dir "$REPO_DIR"
+_is_dir "$WORK_DIR"
+
+
+$WORK_DIR/oe-core/scripts/contrib/patchreview.py -v -b -j status.json $REPO_DIR
 
 # return an error if any malformed patch is found
-cat $KAS_WORK_DIR/build/status.json |
+cat $WORK_DIR/build/status.json |
     python -c "import json,sys;obj=json.load(sys.stdin); sys.exit(1) if 'malformed-sob' in obj[0] or 'malformed-upstream-status' in obj[0] else sys.exit(0)"
